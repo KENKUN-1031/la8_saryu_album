d63d4a49d1f3be9dafdebad9f0a2667ec5944c79
KENKUN-1031 <kencoding1031@gmail.com>
2024-08-24 05:33:14 +0900

    firstCommit

---
+++ app/src/main/java/app/yoshida/saryu/album/PreviewActivity.kt
@@ -0,0 +1,25 @@
+package app.yoshida.saryu.album
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import app.yoshida.saryu.album.databinding.ActivityPreviewBinding
+
+class PreviewActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPreviewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPreviewBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        val imageId = intent.getIntExtra("image", 0)
+        val displayTitle = intent.getStringExtra("title")
+        binding.previewImage.setImageResource(imageId)
+        binding.titleText.text = displayTitle
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+    }
+}
